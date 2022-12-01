cbb5e714aca774994b8e719e3567c7399b30bacf
Nipun Jain <nipunjain119@gmail.com>
2022-12-01 09:23:09 +0530

    created routes, Apis for hotels, users & rooms and models

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import authRoute from './routes/auth.js'
+import hotelsRoute from './routes/hotels.js'
+import usersRoute from './routes/users.js'
+import roomsRoute from './routes/rooms.js'
 
 const app = express();
 dotenv.config();
@@ -21,11 +25,25 @@ mongoose.connection.on("connected", ()=>{
     console.log("mongoDB connected")
 })
 
-app.get('/', (req, res)=>{
-    res.send("Hello world")
-});
+//middlewares
 
+app.use(express.json())
 
+app.use('/api/auth', authRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/rooms', roomsRoute);
+
+app.use((err, req, res, next)=>{
+    const errorStatus = err.status || 500;
+    const errorMessage = req.message || "Something went wrong"
+    return res.status(errorStatus).json({
+        success : false,
+        status : errorStatus,
+        message : errorMessage,
+        stack : err.stack
+    })
+})
 app.listen(3000, ()=>{
     connect();
     console.log("connected to backend");
---
+++ api/routes/hotels.js
@@ -0,0 +1,60 @@
+import express from 'express'
+import Hotel from '../models/Hotel.js';
+import { createError } from '../utils/error.js';
+const router = express.Router();
+
+//CREATE
+router.post('/', async(req, res)=>{
+    const newHotel = new Hotel(req.body);
+
+    try {
+        const savedHotel = await newHotel.save();
+        res.status(200).json(savedHotel)  
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+//UPDATE
+router.put('/:id', async(req, res)=>{
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set : req.body}, {new:true});
+        res.status(200).json(updatedHotel)  
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+//DELETE
+router.delete('/:id', async(req, res)=>{
+    try {
+        await Hotel.findByIdAndDelete(req.params.id);
+        res.status(200).json("hotel has been deleted")  
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+//GET
+router.get('/:id', async(req, res)=>{
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        res.status(200).json(hotel)  
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+//GET ALL
+router.get('/', async(req, res, next)=>{
+    // const failed = true;
+    // if(failed) return next(createError(401, "Sorry user is not authenticated")) 
+    
+    try {
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels)  
+    } catch (err) {
+        next(err);
+    }
+})
+export default router;
